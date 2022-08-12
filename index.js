8f944d39adc7a459967d364cc0d16450dd28ef39
Rohith M <rohithmerala@gmail.com>
2022-08-12 13:20:03 +0530

    fe

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const { Client, GatewayIntentBits} = require("discord.js");
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 require("dotenv").config();
 const commands = require("./commands/cmdHandler");
-const client = new Client({ intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({
+  presence: {
+    activities: [
+      {
+        name: "/help",
+        type: ActivityType.Listening,
+      },
+    ],
+  },
+  intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 module.exports = client;
 
-client.on("ready", () => {
-  commands(client);
-
-  client.user.setPresence({ activities: [{ name: "Rohith M", type: "WATCHING" }] });
-  console.log("Start completed.");
-});
 client.login(process.env.DISCORD_TOKEN);
