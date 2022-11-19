90db45757b45855602da6f2ef4ad7825566cab4b
Rohith M <rohithmerala@gmail.com>
2022-11-19 11:47:54 +0530

    dw

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 require("dotenv").config();
 const commands = require("./commands/cmdHandler");
 const client = new Client({
+  presence: {
+    activities: [
+      {
+        name: "/help",
+        type: ActivityType.Listening,
+      },
+    ],
+  },
   intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 module.exports = client;
